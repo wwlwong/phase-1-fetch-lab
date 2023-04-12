f6318af7f230bffe0b45fa43ce91283aabfb8114
William Wong <william.wai.lim.wong@gmail.com>
2023-04-11 21:28:43 -0400

    phase 1 fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +14,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
