fb3e7c339d468e8408f8a6a18d59845db4d70aa0
William Wong <william.wai.lim.wong@gmail.com>
2023-04-17 12:45:52 -0400

    fetch lab v2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
-  .then(response => response.json())
-  .then(data => renderBooks(data));
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(data => renderBooks(data))
 }
 
 
